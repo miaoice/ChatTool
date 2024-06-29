d59224184c2e019b17deb6933a9cafbe8d3979f8
miaoice <miaouwu@foxmail.com>
2024-06-29 20:08:47 +0800

    Create SendInGamePatch.cs

---
+++ src/Other_code/SendInGamePatch.cs
@@ -0,0 +1,11 @@
+
+namespace ChatTool;
+
+public class SendInGamePatch
+{
+    public static void SendInGame(string text)
+    {
+        if (DestroyableSingleton<HudManager>._instance) 
+            HudManager.Instance.Notifier.AddDisconnectMessage(text);
+    }
+}
