741b4d3fef9a3803bacac316a3386f3f41dacb41
miaoice <miaouwu@foxmail.com>
2024-06-29 20:09:52 +0800

    Create Text.cs

---
+++ src/CT_code/Text.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace ChatTool
+{
+	[HarmonyPatch(typeof(TextBoxTMP))]
+	public class TextBoxPatch
+	{
+		[HarmonyPatch("SetText")]
+		[HarmonyPrefix]
+		public static bool ModifyCharacterLimit(TextBoxTMP __instance, [HarmonyArgument(0)] string input, [HarmonyArgument(1)] string inputCompo = "")
+		{
+			__instance.characterLimit = (AmongUsClient.Instance.AmHost ? 10000 : 10000);
+			if (input.Length < 1)
+			{
+				return true;
+			}
+			int length = input.Length;
+			int num = length - 1;
+			string text = input.Substring(num, length - num);
+			string newValue;
+			if (TextBoxPatch.replaceDic.TryGetValue(text, out newValue))
+			{
+				__instance.SetText(input.Replace(text, newValue), "");
+				return false;
+			}
+			return true;
+		}
+
+		private static Dictionary<string, string> replaceDic = new Dictionary<string, string>
+		{
+			{
+				"（",
+				"("
+			},
+			{
+				"）",
+				")"
+			},
+			{
+				"，",
+				","
+			},
+			{
+				"：",
+				":"
+			},
+			{
+				"[",
+				"【"
+			},
+			{
+				"]",
+				"】"
+			},
+			{
+				"‘",
+				"'"
+			},
+			{
+				"’",
+				"'"
+			},
+			{
+				"“",
+				"''"
+			},
+			{
+				"”",
+				"''"
+			},
+			{
+				"！",
+				"!"
+			}
+		};
+	}
+}
