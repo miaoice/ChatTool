04f0d486a24a58263e816ca7cdf65eadb942039b
miaoice <miaouwu@foxmail.com>
2024-07-07 16:04:49 +0800

    Create OptionItem.cs

---
+++ src/Other_code/key/OptionItem.cs
@@ -0,0 +1,229 @@
+using System;
+using System.Collections.Generic;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace ChatTool
+{
+    public abstract class OptionItem
+    {
+        public static IReadOnlyList<OptionItem> AllOptions => _allOptions;
+        private static List<OptionItem> _allOptions = new();
+        public static int CurrentPreset { get; set; }
+
+        public int Id { get; }
+        public string Name { get; }
+        public int DefaultValue { get; }
+        public TabGroup Tab { get; }
+        public bool IsSingleValue { get; }
+
+        public Color NameColor { get; protected set; }
+        public OptionFormat ValueFormat { get; protected set; }
+        public bool IsHeader { get; protected set; }
+        public bool IsHidden { get; protected set; }
+        public bool IsText { get; protected set; }
+        public Dictionary<string, string> ReplacementDictionary
+        {
+            get => _replacementDictionary;
+            set
+            {
+                if (value == null) _replacementDictionary?.Clear();
+                else _replacementDictionary = value;
+            }
+        }
+        private Dictionary<string, string> _replacementDictionary;
+
+        public int CurrentValue
+        {
+            get => GetValue();
+            set => SetValue(value);
+        }
+
+        public OptionItem Parent { get; private set; }
+        public List<OptionItem> Children;
+
+        public ConfigEntry<int> CurrentEntry =>
+            IsSingleValue ? singleEntry : AllConfigEntries[CurrentPreset];
+        private ConfigEntry<int>[] AllConfigEntries;
+        private ConfigEntry<int> singleEntry;
+
+
+        public OptionBehaviour OptionBehaviour;
+
+        public event EventHandler<UpdateValueEventArgs> UpdateValueEvent;
+
+        public OptionItem(int id, string name, int defaultValue, TabGroup tab, bool isSingleValue)
+        {
+            Id = id;
+            Name = name;
+            DefaultValue = defaultValue;
+            Tab = tab;
+            IsSingleValue = isSingleValue;
+
+            NameColor = Color.white;
+            ValueFormat = OptionFormat.None;
+            IsHeader = false;
+            IsHidden = false;
+            IsText = false;
+
+            Children = new();
+
+            AllConfigEntries = new ConfigEntry<int>[10];
+            if (Id == 1)
+            {
+                singleEntry = Main.Instance.Config.Bind("Current Preset", id.ToString(), DefaultValue);
+                CurrentPreset = singleEntry.Value;
+            }
+            else if (IsSingleValue)
+            {
+                singleEntry = Main.Instance.Config.Bind("SingleEntryOptions", id.ToString(), DefaultValue);
+            }
+            else
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    AllConfigEntries[i] = Main.Instance.Config.Bind($"Preset{i + 1}", id.ToString(), DefaultValue);
+                }
+            }
+
+            _allOptions.Add(this);
+        }
+
+        public OptionItem Do(Action<OptionItem> action)
+        {
+            action(this);
+            return this;
+        }
+
+        public OptionItem SetColor(Color value) => Do(i => i.NameColor = value);
+        public OptionItem SetValueFormat(OptionFormat value) => Do(i => i.ValueFormat = value);
+        public OptionItem SetHeader(bool value) => Do(i => i.IsHeader = value);
+        public OptionItem SetHidden(bool value) => Do(i => i.IsHidden = value);
+        public OptionItem SetText(bool value) => Do(i => i.IsHidden = value);
+
+        public OptionItem SetParent(OptionItem parent) => Do(i =>
+        {
+            i.Parent = parent;
+            parent.SetChild(i);
+        });
+        public OptionItem SetChild(OptionItem child) => Do(i => i.Children.Add(child));
+        public OptionItem RegisterUpdateValueEvent(EventHandler<UpdateValueEventArgs> handler)
+            => Do(i => UpdateValueEvent += handler);
+
+        public OptionItem AddReplacement((string key, string value) kvp)
+            => Do(i =>
+            {
+                ReplacementDictionary ??= new();
+                ReplacementDictionary.Add(kvp.key, kvp.value);
+            });
+        public OptionItem RemoveReplacement(string key)
+            => Do(i => ReplacementDictionary?.Remove(key));
+
+        public virtual bool GetBool() => CurrentValue != 0 && (Parent == null || Parent.GetBool());
+        public virtual string GetString()
+        {
+            return ApplyFormat(CurrentValue.ToString());
+        }
+        public virtual int GetValue() => CurrentEntry.Value;
+
+
+        public string ApplyFormat(string value)
+        {
+            if (ValueFormat == OptionFormat.Players) return value + " players";
+            if (ValueFormat == OptionFormat.Seconds) return value + "s";
+            if (ValueFormat == OptionFormat.Percent) return value + "%";
+            if (ValueFormat == OptionFormat.Multiplier) return value + "x";
+            if (ValueFormat == OptionFormat.PerSecond) return value + "/s";
+            if (ValueFormat == OptionFormat.PerLevel) return value + "/lvl";
+            if (ValueFormat == OptionFormat.Resources) return value + " res";
+            if (ValueFormat == OptionFormat.Money) return value + "$";
+            if (ValueFormat == OptionFormat.PercentPerLevel) return value + "%/lvl";
+            if (ValueFormat == OptionFormat.ResourcesPerLevel) return value + " res/lvl";
+            if (ValueFormat == OptionFormat.MoneyPerLevel) return value + "$/lvl";
+            return value;
+        }
+
+        public virtual void Refresh()
+        {
+            if (OptionBehaviour is not null and StringOption opt)
+            {
+                opt.ValueText.text = GetString();
+                opt.oldValue = opt.Value = CurrentValue;
+            }
+        }
+        public virtual void SetValue(int value)
+        {
+            int beforeValue = CurrentEntry.Value;
+            int afterValue = CurrentEntry.Value = value;
+
+            CallUpdateValueEvent(beforeValue, afterValue);
+            Refresh();
+            SyncAllOptions();
+        }
+
+        public static OptionItem operator ++(OptionItem item)
+            => item.Do(item => item.SetValue(item.CurrentValue + 1));
+        public static OptionItem operator --(OptionItem item)
+            => item.Do(item => item.SetValue(item.CurrentValue - 1));
+
+        public static void SwitchPreset(int newPreset)
+        {
+            CurrentPreset = Math.Clamp(newPreset, 0, 9);
+
+            foreach (var op in AllOptions)
+                op.Refresh();
+
+            SyncAllOptions();
+        }
+        public static void SyncAllOptions()
+        {
+            if (
+                PlayerControl.AllPlayerControls.Count <= 1 ||
+                AmongUsClient.Instance.AmHost == false ||
+                PlayerControl.LocalPlayer == null
+            ) return;
+
+        }
+
+
+        private void CallUpdateValueEvent(int beforeValue, int currentValue)
+        {
+            if (UpdateValueEvent == null) return;
+            UpdateValueEvent(this, new UpdateValueEventArgs(beforeValue, currentValue));
+        }
+
+        public class UpdateValueEventArgs : EventArgs
+        {
+            public int CurrentValue { get; set; }
+            public int BeforeValue { get; set; }
+            public UpdateValueEventArgs(int beforeValue, int currentValue)
+            {
+                CurrentValue = currentValue;
+                BeforeValue = beforeValue;
+            }
+        }
+    }
+
+    public enum TabGroup
+    {
+        ModSettings,
+        GamemodeSettings,
+        AdditionalGamemodes,
+    }
+    
+    public enum OptionFormat
+    {
+        None,
+        Players,
+        Seconds,
+        Percent,
+        Multiplier,
+        PerSecond,
+        PerLevel,
+        Resources,
+        Money,
+        PercentPerLevel,
+        ResourcesPerLevel,
+        MoneyPerLevel,
+    }
+}
