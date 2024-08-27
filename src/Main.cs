762004c78f467d5456e0e01690c618eee3ab4993
miaoice <miaouwu@foxmail.com>
2024-08-27 15:28:03 +0800

    Update Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
+using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 
+[assembly: AssemblyFileVersion(ChatTool.Main.PluginVersion)]
+[assembly: AssemblyVersion(ChatTool.Main.PluginVersion)]
 namespace ChatTool;
-
 [BepInProcess("Among Us.exe")]
+
 public class Main : BasePlugin
 {
 
     public static readonly string ModName = "ChatTool"; // 模组名字
-    public const string PluginVersion = "1.2.0"; //版本号
+    public const string PluginVersion = "1.2.1"; //版本号
     public static BepInEx.Logging.ManualLogSource Logger;
     
     public static Main Instance; //设置Main实例
@@ -32,14 +34,14 @@ public class Main : BasePlugin
     public static readonly string ForkId = "ChatTool";
     public static Dictionary<byte, PlayerVersion> playerVersion = new();
     
-
-
 }
 
 public class PlayerVersion
 {
     public readonly Version version;
+
     public readonly string tag;
+    
     public readonly string forkId;
     public PlayerVersion(string ver, string tag_str, string forkId) : this(Version.Parse(ver), tag_str, forkId) { }
     public PlayerVersion(Version ver, string tag_str, string forkId)
